46fae2ce7ef42f63e971114c5cf23292f3d8836d
Wu Xiaoyu <wuxiaoyu@mail.ustc.edu.cn>
2025-06-17 15:35:27 +0800

    a new version
    I have no idea how many new features were added. Enjoy :)

--- xiaoyu_CF.py
+++ xiaoyu_CF.py
@@ -3,12 +3,18 @@ import glob
 import os.path
 import numpy as np
 
+# suppress warnings
+# ZE5 has some problems with the data output, which will cause some warnings.
+# but the warnings are not important for the data analysis.
+# and the data is just 4 order of magnitude larger than it should be.
 import warnings
 warnings.filterwarnings("ignore")
 
 class xiaoyu_Expr:
 
-    def __init__(self, working_dir : str):
+    # load fcs files in the working directory
+    # usage: xiaoyu_Expr("working_directory", ["merge_directory1", "merge_directory2", ...])
+    def __init__(self, working_dir : str, merge_dirs = []):
         self.working_dir = ""
         self.filelist = []
         self.name_locus_mapping = {}
@@ -21,6 +27,17 @@ class xiaoyu_Expr:
         self.filelist = filelist
         print (f"{len(filelist)} fsc files loaded")
 
+        # load other fcs files in the merge_dirs, and merge them to the current experiment by well index.
+        # this is useful when you have some additive data to the current experiment.
+        if len(merge_dirs) != 0:
+            for merge_dir in merge_dirs:
+                if merge_dir[-1] != "/":
+                    merge_dir += "/"
+                filelist_merge = glob.glob(merge_dir + "*.fcs")
+                filelist_merge = sorted(filelist_merge)
+                self.filelist += filelist_merge
+                print (f"other {len(filelist_merge)} fsc files loaded from {merge_dir}. will be merged to the current experiment by well index.")
+
         # classify samples with name (A1, A2, A3, ..., B1, B2, B3, ...)
         file_names = []
         for i in filelist:
@@ -29,9 +46,13 @@ class xiaoyu_Expr:
         for i in range(1, len(file_names)+1):
             self.name_locus_mapping[i] = [file_names[i-1][0], int(file_names[i-1][1:])-1]
         #print (f"mapping of sample name to char and num: {name_locus_mapping}")
+
+        # for merge_dir in merge_dirs: ------------------------------------------------------------- function addition: merge some additive data to the current experiment
+            
         # generate experiment instance
         tubes = []
         count = 1
+        # for merged data, the sample index (num, char, well) should be the same with the original data, but the sample index (sample) should be different for clarification.
         for i in filelist:
             tube = cytoflow.Tube(file = i, conditions = {"sample" : count, "char" : self.name_locus_mapping[count][0], "num" : str(self.name_locus_mapping[count][1]+1), "well" : self.name_locus_mapping[count][0]+str(self.name_locus_mapping[count][1]+1)})
             tubes.append(tube)
@@ -41,6 +62,11 @@ class xiaoyu_Expr:
         self.expr = experiment
 
     # add concetration condition to the experiment
+    # usage: add_conc_condition(ori_conc, dilu_dir = "down", dilu_factor = 5)
+    # this function will add a new condition to the experiment with the name "conc" and the dtype "float".
+    # the concentration of the samples will be assigned according to the dilution direction and dilution factor.
+    # the concentration of the first sample will be the ori_conc, and the concentration of the other samples will be calculated by the dilution factor.
+    # this sample only works for 96-well plate.
     def add_conc_condition(self, ori_conc, dilu_dir = "down", dilu_factor = 5):
         conc = []
         condition_list = []
@@ -66,14 +92,39 @@ class xiaoyu_Expr:
             self.expr.add_condition(name = "conc", dtype = "float", data = condition_list)
 
     # add other conditions
+    # usage: add_condition_with_sample_array("condition_name", "dtype", [condition1, condition2, condition3, ...])
+    # this function will add a new condition to the experiment with the name "condition_name" and the dtype "dtype".
+    # by assigning the list of conditions to their corresponding samples.
+    # the length of the conditions should be the same with the number of samples in the experiment.
+    # if module tabulate is installed, the mapping of the sample index to the new condition value will be shown.
     def add_condition_with_sample_array(self, condition_name, dtype, conditions):
         condition_list = []
         for i in self.expr.data["sample"]:
             condition_list.append( conditions[i-1] )
         condition_list = np.array(condition_list)
         self.expr.add_condition(name = condition_name, dtype = dtype, data = condition_list)
+        print (f"condition {condition_name} added to the experiment")
+        print ("mapping of the sample index to the new condition value is shown below:")
+        try:
+            from tabulate import tabulate
+            print (tabulate([[i, f"{self.name_locus_mapping[i][0]}{self.name_locus_mapping[i][1]+1}", conditions[i-1]] for i in range(1, len(self.name_locus_mapping)+1)], headers = ["sample", "well", condition_name], tablefmt='fancy_grid'))
+        except:
+            print ("tabulate module not found. please install tabulate module to show the mapping of the sample index to the new condition value.")
 
+    def sample_index_to_well_index_output(self):
+        try:
+            from tabulate import tabulate
+            print (tabulate([[i, f"{self.name_locus_mapping[i][0]}{self.name_locus_mapping[i][1]+1}"] for i in range(1, len(self.name_locus_mapping)+1)], headers = ["sample", "well"], tablefmt='fancy_grid'))
+        except:
+            print ("tabulate module not found. please install tabulate module to show the mapping of the sample index to the well index.")
+    
     # median value output
+    # usage: median_96well(experiment, channel = "Alexa 647-A", interval = "\t")
+    # this function will output the median values of the channel in the 96-well plate format.
+    # the output will be saved to the working directory with the name "median_values.txt".
+    # the interval between the values can be set by the parameter "interval".
+    # the median values will also be returned as a 2D list.
+    # this function only works for 96-well plate.
     def median_96well(self, experiment, channel = "Alexa 647-A", interval = "\t"):
         median_values_raw = experiment.data.groupby('sample')[channel].median()
         median_values = [[0 for i in range(12)] for j in range(8)]
@@ -86,21 +137,24 @@ class xiaoyu_Expr:
             file.write("\n")
         file.close()
         print (f"median values of {channel} output to {self.working_dir}median_values.txt")
-        return
+        return median_values
 
 # gate out outliers
 def gate_outliers(experiment, channel = "Alexa 647-A", if_plot = False, low = 2000, high = 2147483647):
     gate = cytoflow.RangeOp(name = "de_outliers", channel = channel, low = low, high = high)
     if if_plot:
-        gate.default_view().plot(experiment)
+        cytoflow.HistogramView(channel = channel, scale = "log", huefacet = "well").plot(experiment, num_bins = 200, density = True, title = f"Histogram of {channel}")
     return gate.apply(experiment).subset("de_outliers", True)
 
 # assist auto gate
+assistants = []
 def assist_gate(experiment, channel = "FSC 488/10-A", if_plot = False, low = 2000, high = 2147483647):
-    gate = cytoflow.RangeOp(name = "assist", channel = channel, low = low, high = high)
+    global assistants
+    gate = cytoflow.RangeOp(name = f"assist{len(assistants)+1}", channel = channel, low = low, high = high)
+    assistants.append(gate)
     if if_plot:
-        gate.default_view().plot(experiment)
-    return gate.apply(experiment).subset("assist", True)
+        gate.default_view(scale = "log").plot(experiment, title = channel)
+    return gate.apply(experiment).subset(f"assist{len(assistants)}", True)  # without +1 because already conducted appending
 
 # plot FSC-A ~ SSC-A
 def FSC_SSC_ploting(experiment, type = "scatter"):
@@ -122,7 +176,7 @@ def gate_FSC_SSC(experiment, vertices, if_plot = False):
 def auto_gate_FSC_SSC(experiment, if_plot = False, keep = 0.6):
     dens_gate = cytoflow.DensityGateOp(name = "auto_FSCA_SSCA", xchannel = "FSC 488/10-A", ychannel = "SSC 488/10-A", xscale = "linear", yscale = "linear", keep = keep)
     dens_gate.estimate(experiment)
-    if if_plot:        
+    if if_plot:
         dens_gate.default_view().plot(experiment)
     return dens_gate.apply(experiment).subset("auto_FSCA_SSCA", True)
 
@@ -147,19 +201,27 @@ def auto_gate_FSC_A_H(experiment, if_plot = False, keep = 0.9):
         dens_gate.default_view().plot(experiment)
     return dens_gate.apply(experiment).subset("auto_FSCA_FSCH", True)
 
+# make scatter plot
+def log_density_plot(experiment, xchannel = "FSC 488/10-A", ychannel = "FSC 488/10-H", huefacet = "", **kwargs):
+    if huefacet != "":
+        cytoflow.DensityView(xchannel = xchannel, ychannel = ychannel, xscale = "log", yscale = "log", huefacet = huefacet).plot(experiment, **kwargs)
+    else:
+        cytoflow.DensityView(xchannel = xchannel, ychannel = ychannel, xscale = "log", yscale = "log").plot(experiment, **kwargs)
+    return
+
 # plot histogram of a channel
-def channel_histogram(experiment, channel = "Alexa 647-A", huefacet = "sample", bins = 200, title = ""):
+def channel_histogram(experiment, channel = "Alexa 647-A", huefacet = "sample", bins = 200, title = "", **kwargs):
     if title == "":
         if huefacet == "conc":
-            cytoflow.HistogramView(channel = channel, scale = "log", huefacet = huefacet, huescale = "log").plot(experiment, num_bins = bins, alpha = 0.9, density = True)
+            cytoflow.HistogramView(channel = channel, scale = "log", huefacet = huefacet, huescale = "log").plot(experiment, num_bins = bins, alpha = 0.9, density = True, **kwargs)
         else:
-            cytoflow.HistogramView(channel = channel, scale = "log", huefacet = huefacet).plot(experiment, num_bins = bins, density = True)
+            cytoflow.HistogramView(channel = channel, scale = "log", huefacet = huefacet).plot(experiment, num_bins = bins, density = True, **kwargs)
         return
     else:
         if huefacet == "conc":
-            cytoflow.HistogramView(channel = channel, scale = "log", huefacet = huefacet, huescale = "log").plot(experiment, num_bins = bins, alpha = 0.9, density = True, title = title)
+            cytoflow.HistogramView(channel = channel, scale = "log", huefacet = huefacet, huescale = "log").plot(experiment, num_bins = bins, alpha = 0.9, density = True, title = title, **kwargs)
         else:
-            cytoflow.HistogramView(channel = channel, scale = "log", huefacet = huefacet).plot(experiment, num_bins = bins, density = True, title = title)
+            cytoflow.HistogramView(channel = channel, scale = "log", huefacet = huefacet).plot(experiment, num_bins = bins, density = True, title = title, **kwargs)
         return
 
 # get sub set by char
@@ -171,3 +233,20 @@ def subset_by_char(exper, char):
 def subset_by_num(exper, num):
     subset = exper.subset('num', num)
     return subset
+
+# get sub set by well index
+def subset_by_well(exper, well):
+    if (type(well) == str):
+        subset = exper.subset('well', well)
+        return subset
+    elif (type(well) == list):
+        subset = exper.clone(deep = False)
+        subset.data = exper.data[exper.data['well'].isin(well)]
+        subset.data.reset_index(drop = True, inplace = True)
+        return subset
+    else:
+        raise TypeError("well should be a string or a list of strings, but got " + str(type(well)))
+
+# get count of cells in the experiment
+def cell_count(experiment):
+    return len(experiment.data)
